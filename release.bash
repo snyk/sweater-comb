771471128221234a719c09fd2fa41898bafb380d
Casey Marshall <casey.marshall@snyk.io>
2021-08-31 09:15:52 -0500

    chore: modularize rules
    Break up rules into reusable, reference-able parts.
    
    Drive-by: add release script.
    Drive-by: fix jsonapi rule (property is all lowercase)
    Drive-by: exceptions for /openapi endpoints

---
+++ release.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eux
+cd $(dirname $0)
+
+NOW=$(date '+%Y%m%d%H%M')
+COMMIT=$(git rev-parse --short HEAD)
+export VERSION=$(git describe --tags --abbrev=0)+${NOW}-${COMMIT}
+
+tmp_package=$(mktemp)
+trap "rm -f $tmp_package" EXIT
+
+jq -r ".version = \"$VERSION\"" package.json > $tmp_package
+mv $tmp_package package.json
