9cea8b532ed8843036f408f10214140f163ba1a2
Graham Smith <g@blaxian.com>
2021-10-06 12:28:34 +0100

    feat: prevent compound documents from being returned in a response
    JSON:API contains the idea of [compound documents](https://jsonapi.org/format/#document-compound-documents) which we have decided we do not want to allow in our version of JSON:API
    Here we lint for the existence of the `included` field in the response and fail if it is detected.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,13 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "jest": true,
-        "node": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-    }
+  env: {
+    commonjs: true,
+    jest: true,
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
 };
