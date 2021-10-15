3ebfbe278225677fcc02e192c61edc27a751dca9
Graham Smith <g@blaxian.com>
2021-10-15 17:02:28 +0100

    feat: rules to standardize create, updated and deleted responses

--- functions/assertObjectPath.js
+++ functions/assertObjectPath.js
@@ -1,8 +1,6 @@
-const camelCaseRegex = /^([a-z]+([A-Z][a-z]+)*)$/;
-
-module.exports = (targetVal, opts, context) => {
-  for (var i = 0; i < opts.path.length; i++) {
-    targetVal = targetVal[opts.path[i]];
+module.exports = (targetVal, opts) => {
+  for (let item of opts.path) {
+    targetVal = targetVal && targetVal[item];
     if (!targetVal) {
       break;
     }
