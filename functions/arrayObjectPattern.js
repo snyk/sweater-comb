a436669def69600c0453568fcbbb06bf0726b4c3
Graham Smith <g@blaxian.com>
2021-10-19 12:13:04 +0100

    feat: standardize pagination params and links
    Enforce pagination parameters and responses on collection requests.
    Prohibit pagination parameters on post/patch/delete endpoints.

--- functions/arrayObjectPattern.js
+++ functions/arrayObjectPattern.js
@@ -1,10 +1,9 @@
-const camelCaseRegex = /^([a-z]+([A-Z][a-z]+)*)$/;
-
-module.exports = (targetVal, opts, context) => {
+module.exports = (targetVal, opts) => {
   const re = new RegExp(opts.match);
   const result = targetVal.filter((item) => {
     return item[opts.field].match(re);
   });
+
   if (!result || result.length === 0) {
     return [
       {
