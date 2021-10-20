bc088598a7850a1901216145338439dc8e1d09e9
Dmitry Demenchuk <dmitry@demenchuk.me>
2021-10-20 12:19:37 +0100

    chore: replace usage of fixtures

--- tests/arrayTypes.test.js
+++ tests/arrayTypes.test.js
@@ -1,16 +1,90 @@
 const { Spectral } = require('@stoplight/spectral-core');
-const { loadRules, loadSpec } = require('./utils');
+const { specFactory, loadOneRule } = require('./utils');
 
-let rules;
+let rules, spectral;
+const ruleName = 'openapi-arrays-types';
 
 beforeAll(async () => {
-  rules = await loadRules('spec.yaml');
+  const rule = await loadOneRule('spec.yaml', ruleName);
+
+  rules = {
+    rules: { [ruleName]: rule },
+  };
 });
 
-it('fails on array having no type rule', async () => {
-  const spectral = new Spectral();
+beforeEach(async () => {
+  spectral = new Spectral();
   spectral.setRuleset(rules);
-  const result = await spectral.run(loadSpec('fixtures/arrayTypes.fail.yaml'));
+});
+
+it('passes if array has a type field', async () => {
+  // arrange
+  const path = {
+    get: {
+      description: 'example path',
+      operationId: 'getExample',
+      parameters: [
+        {
+          in: 'query',
+          name: 'tags',
+          schema: {
+            type: 'array',
+            items: {
+              pattern: '^[a-zA-Z0-9_-]+:[a-zA-Z0-9_-]+$',
+              type: 'string',
+            },
+          },
+        },
+      ],
+      responses: {
+        200: { description: 'OK' },
+      },
+    },
+  };
+
+  const spec = specFactory({
+    paths: { '/example': path },
+  });
+
+  // act
+  const result = await spectral.run(spec);
+
+  // assert
+  expect(result).toHaveLength(0);
+});
+
+it("fails if array doesn't have a type field", async () => {
+  // arrange
+  const path = {
+    get: {
+      description: 'example path',
+      operationId: 'getExample',
+      parameters: [
+        {
+          in: 'query',
+          name: 'tags',
+          schema: {
+            type: 'array',
+            items: {
+              pattern: '^[a-zA-Z0-9_-]+:[a-zA-Z0-9_-]+$',
+            },
+          },
+        },
+      ],
+      responses: {
+        200: { description: 'OK' },
+      },
+    },
+  };
+
+  const spec = specFactory({
+    paths: { '/example': path },
+  });
+
+  // act
+  const result = await spectral.run(spec);
+
+  // assert
   expect(result).not.toHaveLength(0);
   expect(result).toEqual(
     expect.arrayContaining([
@@ -19,10 +93,10 @@ it('fails on array having no type rule', async () => {
         message: 'Array items must have a "type" field.',
         path: [
           'paths',
-          '/groups/{group_id}/issues',
+          '/example',
           'get',
           'parameters',
-          '2',
+          '0',
           'schema',
           'items',
         ],
