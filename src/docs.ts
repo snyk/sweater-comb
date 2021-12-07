053ca9422a93c4f3bb24467316a9b5a9e74ddfa3
Stephen Mizell <smizell@gmail.com>
2021-12-07 12:46:36 -0600

    Add doc links for lifecycle, headers, and jsonapi rules

---
+++ src/docs.ts
@@ -0,0 +1,27 @@
+export const links = {
+  versioning: {
+    main: "https://github.com/snyk/sweater-comb/blob/main/docs/version.md",
+    promotingStability:
+      "https://github.com/snyk/sweater-comb/blob/main/docs/version.md#promoting-stability-of-a-resource-over-time",
+    stabilityLevels:
+      "https://github.com/snyk/sweater-comb/blob/main/docs/version.md#stability-levels",
+    responseHeaders:
+      "https://github.com/snyk/sweater-comb/blob/main/docs/version.md#versioning-response-headers",
+  },
+  standards: {
+    statusCodes:
+      "https://github.com/snyk/sweater-comb/blob/main/docs/standards.md#status-codes",
+    headers: {
+      case: "https://github.com/snyk/sweater-comb/blob/main/docs/standards.md#header-field-names",
+    },
+  },
+  jsonApi: {
+    contentType: "https://jsonapi.org/format/#content-negotiation-clients",
+    resourceObjects: "https://jsonapi.org/format/#document-resource-objects",
+    resourceObjectLinks:
+      "https://jsonapi.org/format/#document-resource-object-links",
+    pagination: "https://jsonapi.org/format/#fetching-pagination",
+    compoundDocuments:
+      "https://github.com/snyk/sweater-comb/blob/main/docs/jsonapi.md#compound-documents",
+  },
+};
