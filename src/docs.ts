b4e478fdb9b10bd4dd223a5f9b0c9417b72e6ca8
Stephen Mizell <smizell@gmail.com>
2021-12-07 15:20:55 -0600

    Complete adding doc links

--- src/docs.ts
+++ src/docs.ts
@@ -7,6 +7,10 @@ export const links = {
       "https://github.com/snyk/sweater-comb/blob/main/docs/version.md#stability-levels",
     responseHeaders:
       "https://github.com/snyk/sweater-comb/blob/main/docs/version.md#versioning-response-headers",
+    breakingChanges:
+      "https://github.com/snyk/sweater-comb/blob/main/docs/version.md#breaking-changes",
+    versionParameter:
+      "https://github.com/snyk/sweater-comb/blob/main/docs/version.md#how-are-versions-accessed-and-resolved-by-consumers",
   },
   standards: {
     statusCodes:
@@ -14,6 +18,19 @@ export const links = {
     headers: {
       case: "https://github.com/snyk/sweater-comb/blob/main/docs/standards.md#header-field-names",
     },
+    referencedEntities:
+      "https://github.com/snyk/sweater-comb/blob/main/docs/standards.md#referenced-entities",
+    openApiVersions:
+      "https://github.com/snyk/sweater-comb/blob/main/docs/standards.md#making-the-openapi-specification-available",
+    orgAndGroupTenantResources:
+      "https://github.com/snyk/sweater-comb/blob/main/docs/standards.md#organization-and-group-tenants-for-resources",
+    tags: "https://github.com/snyk/sweater-comb/blob/main/docs/standards.md#tags",
+    formats:
+      "https://github.com/snyk/sweater-comb/blob/main/docs/standards.md#formats",
+    operationIds:
+      "https://github.com/snyk/sweater-comb/blob/main/docs/standards.md#operation-ids",
+    parameterNamesPathComponents:
+      "https://github.com/snyk/sweater-comb/blob/main/docs/standards.md#parameter-names-and-path-components",
   },
   jsonApi: {
     contentType: "https://jsonapi.org/format/#content-negotiation-clients",
