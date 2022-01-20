fec7a5c71db8475d70db840166082b19d346dea5
Nicholas <18374483+niclim@users.noreply.github.com>
2022-01-20 16:44:50 -0500

    Update optic to 0.21.3
    Signed-off-by: Nicholas <18374483+niclim@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,13 @@ import { makeCiCli } from "@useoptic/api-checks/build/ci-cli/make-cli";
 import { newSnykApiCheckService } from "./service";
 
 const apiCheckService = newSnykApiCheckService();
-const cli = makeCiCli("sweater-comb", apiCheckService);
+const cli = makeCiCli("sweater-comb", apiCheckService, {
+  opticToken: process.env.OPTIC_TOKEN || '',
+  gitProvider: {
+    token: process.env.GITHUB_TOKEN || '',
+    provider: 'github',
+  },
+  ciProvider: 'circleci',
+});
 
 cli.parse(process.argv);
