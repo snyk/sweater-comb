b14108b79eb8cd488fea90462c2fb6e2025458da
Darrell Mozingo <darrell@mozingo.net>
2022-01-31 16:29:11 -0500

    feat: display untenanted urls in error message
    Invert what we're looking for (invalid, rather than valid), so the error
    message is more helpful by displaying naughty urls.

--- src/rulesets/specification.ts
+++ src/rulesets/specification.ts
@@ -53,16 +53,15 @@ export const rules = {
       (spec, context, docs) => {
         docs.includeDocsLink(links.standards.orgAndGroupTenantResources);
         docs.becomesEffectiveOn(new Date("2021-07-01"));
-        const paths = Object.keys(spec.paths);
-        const tenantUrls = paths.filter(
+        const untenantedUrls = Object.keys(spec.paths).filter(
           (url) =>
-            url.startsWith("/orgs/{org_id}") ||
-            url.startsWith("/groups/{group_id}"),
+            !url.startsWith("/orgs/{org_id}") &&
+            !url.startsWith("/groups/{group_id}"),
         );
         expect(
-          tenantUrls,
+          untenantedUrls,
           `expected support for org or group tenant`,
-        ).to.have.lengthOf(paths.length);
+        ).to.have.lengthOf(0);
       },
     );
   },
