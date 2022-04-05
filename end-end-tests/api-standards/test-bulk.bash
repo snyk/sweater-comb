d80d54050af97dd5af48b071a8a96a71163495ee
Aidan Cunniffe <acunniffe@gmail.com>
2022-04-05 07:58:11 -0400

    feat: singleton and batch support

--- end-end-tests/api-standards/test-bulk.bash
+++ end-end-tests/api-standards/test-bulk.bash
@@ -31,6 +31,8 @@ cat >$tempdir/passes <<EOF
     }, {
         "from": "$HERE/resources/thing/2021-11-10/000-fail-naming.yaml", "to": "$HERE/resources/thing/2021-11-10/000-fail-naming.yaml", "context": {"changeDate": "2021-11-11", "changeResource": "thing", "changeVersion": {"date": "2021-11-10", "stability": "experimental"}}
     }, {
+        "from": "$HERE/resources/thing/2021-11-10/001-singleton.yaml", "to": "$HERE/resources/thing/2021-11-10/001-singleton.yaml", "context": {"changeDate": "2021-11-11", "changeResource": "thing", "changeVersion": {"date": "2021-11-10", "stability": "experimental"}}
+    }, {
         "from": "$HERE/resources/thing/2021-11-10/000-baseline-in-reform.yaml", "to": "$HERE/resources/thing/2021-11-10/002-fail-tenancy.yaml", "context": {"changeDate": "2021-11-11", "changeResource": "thing", "changeVersion": {"date": "2021-11-10", "stability": "experimental"}}
     }]
 }
@@ -68,6 +70,8 @@ cat >$tempdir/failures <<EOF
         "from": "$HERE/resources/thing/2021-11-10/002-ok-add-operation.yaml", "to": "$HERE/resources/thing/2021-11-10/003-fail-type-change.yaml", "context": {"changeDate": "2021-11-11", "changeResource": "thing", "changeVersion": {"date": "2021-11-10", "stability": "experimental"}}
     }, {
         "from": "$HERE/resources/thing/2021-11-10/001-ok-add-property-field.yaml", "to": "$HERE/resources/thing/2021-11-10/002-fail-tenancy.yaml", "context": {"changeDate": "2021-11-11", "changeResource": "thing", "changeVersion": {"date": "2021-11-10", "stability": "experimental"}}
+    }, {
+        "from": "$HERE/resources/thing/2021-11-10/002-fail-singleton.yaml", "to": "$HERE/resources/thing/2021-11-10/002-fail-singleton.yaml", "context": {"changeDate": "2021-11-11", "changeResource": "thing", "changeVersion": {"date": "2021-11-10", "stability": "experimental"}}
     }, {
         "from": "$HERE/resources/thing/2021-11-10/000-baseline.yaml", "to": "$HERE/resources/thing/2021-11-10/003-jsonapi.yaml", "context": {"changeDate": "2021-11-11", "changeResource": "thing", "changeVersion": {"date": "2021-11-10", "stability": "experimental"}}
     }]
