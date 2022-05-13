89b4f44ffa81d45074767d6c7073be532ddb5e98
Nicholas <18374483+niclim@users.noreply.github.com>
2022-05-13 10:11:51 -0400

    chore: migrate lifecycle rules to rule authoring v2

---
+++ src/rulesets-new/__tests__/fixtures.ts
@@ -0,0 +1,9 @@
+export const context = {
+  changeDate: "2021-10-10",
+  changeResource: "Example",
+  changeVersion: {
+    date: "2021-10-10",
+    stability: "ga",
+  },
+  resourceVersions: {},
+};
