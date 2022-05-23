0bfb11c5f0811340386daf101054c9b2a3565ea4
Nicholas <18374483+niclim@users.noreply.github.com>
2022-05-23 12:43:34 -0400

    chore: connect new rules to optic-ci runner

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { initializeCli } from "@useoptic/optic-ci/build/initialize";
-import { newSnykApiCheckService } from "./service";
+import { rules } from './rulesets-new'
 import { Command } from "commander";
 import {
   createResourceCommand,
@@ -14,7 +14,6 @@ import {
   createUpdateCommand,
 } from "./workflows/commands";
 
-const apiCheckService = newSnykApiCheckService();
 
 (async () => {
   const cli = await initializeCli({
@@ -22,13 +21,7 @@ const apiCheckService = newSnykApiCheckService();
     gitProvider: {
       token: process.env.GITHUB_TOKEN || "",
     },
-    checks: [
-      {
-        name: "snyk-checks",
-        type: "custom",
-        checkService: apiCheckService,
-      },
-    ],
+    rules: rules,
   });
 
   const workflowCommand = new Command("workflow").description(
