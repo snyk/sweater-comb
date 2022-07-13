b082192721182da3fce7153fc1bf1ab6083ca65e
JC Sackett <jc.sackett@snyk.io>
2022-07-13 17:20:07 +0100

    feat: updates optic libs

--- src/rulesets/rest/2022-05-25/__tests__/end-end-compiled.test.ts
+++ src/rulesets/rest/2022-05-25/__tests__/end-end-compiled.test.ts
@@ -1,10 +1,7 @@
 import path from "path";
 import { RuleRunner, TestHelpers } from "@useoptic/rulesets-base";
-import { ResultWithSourcemap } from "@useoptic/openapi-utilities";
-import {
-  sourcemapReader,
-  parseOpenAPIWithSourcemap,
-} from "@useoptic/openapi-io";
+import { ResultWithSourcemap, sourcemapReader } from "@useoptic/openapi-utilities";
+import { parseOpenAPIWithSourcemap } from "@useoptic/openapi-io";
 import { compiledRules as rules } from "../index";
 
 const ruleRunner = new RuleRunner(rules);
