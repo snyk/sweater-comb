3032aafbbe7846d03a27c7c8fce79f746be699e2
Casey Marshall <casey.marshall@snyk.io>
2022-07-20 11:39:17 -0500

    feat: sweater-comb lint command
    Lint command which frontends optic-ci's bulk-compare, using the
    project's .vervet.yaml to locate resource OpenAPI specs and determine
    the base branch when not given as arguments.

--- src/woollypully/__tests__/checker.test.ts
+++ src/woollypully/__tests__/checker.test.ts
@@ -7,6 +7,9 @@ import { Checker } from "../checker";
 
 import { getConfig } from "../config";
 
+// Tests which spawn subprocesses seem to take longer to execute in CircleCI
+const testTimeout = 30000;
+
 jest.mock("axios");
 jest.mock("@useoptic/openapi-io");
 
@@ -43,42 +46,50 @@ describe("checker", () => {
     jest.resetAllMocks();
   });
 
-  test("can check APIs", async () => {
-    (axios.default.create as jest.Mock).mockImplementation(() => ({
-      get: jest
-        .fn()
-        .mockImplementationOnce(() =>
-          Promise.resolve({ data: defaultApis, status: 200 }),
-        )
-        .mockImplementationOnce(() =>
-          Promise.resolve({ data: defaultVersions, status: 200 }),
-        )
-        .mockImplementationOnce(() =>
-          Promise.resolve({ data: defaultSpecJson, status: 200 }),
-        ),
-    }));
-    const checker = new Checker(await getConfig());
-    try {
-      await checker.checkApis();
-    } catch (err: any) {
-      fail(err.message);
-    }
-  }, 30000);
+  test(
+    "can check APIs",
+    async () => {
+      (axios.default.create as jest.Mock).mockImplementation(() => ({
+        get: jest
+          .fn()
+          .mockImplementationOnce(() =>
+            Promise.resolve({ data: defaultApis, status: 200 }),
+          )
+          .mockImplementationOnce(() =>
+            Promise.resolve({ data: defaultVersions, status: 200 }),
+          )
+          .mockImplementationOnce(() =>
+            Promise.resolve({ data: defaultSpecJson, status: 200 }),
+          ),
+      }));
+      const checker = new Checker(await getConfig());
+      try {
+        await checker.checkApis();
+      } catch (err: any) {
+        fail(err.message);
+      }
+    },
+    testTimeout,
+  );
 
-  test("fails if service is unavailable", async () => {
-    (axios.default.create as jest.Mock).mockImplementation(() => ({
-      get: jest
-        .fn()
-        .mockImplementation(() => Promise.reject(new Error("bad wolf"))),
-    }));
-    const checker = new Checker(await getConfig());
-    try {
-      await checker.checkApis();
-      fail();
-    } catch (err: any) {
-      expect(err.message).toMatch("bad wolf");
-    }
-  });
+  test(
+    "fails if service is unavailable",
+    async () => {
+      (axios.default.create as jest.Mock).mockImplementation(() => ({
+        get: jest
+          .fn()
+          .mockImplementation(() => Promise.reject(new Error("bad wolf"))),
+      }));
+      const checker = new Checker(await getConfig());
+      try {
+        await checker.checkApis();
+        fail();
+      } catch (err: any) {
+        expect(err.message).toMatch("bad wolf");
+      }
+    },
+    testTimeout,
+  );
 
   test.each([400, 404, 500])(
     "fails if apis request responds with %s",
@@ -98,6 +109,7 @@ describe("checker", () => {
         expect(err.message).toMatch("failed to obtain APIs");
       }
     },
+    testTimeout,
   );
 
   test.each([400, 404, 500])(
@@ -121,51 +133,60 @@ describe("checker", () => {
         expect(err.message).toMatch("failed to obtain OpenAPI versions");
       }
     },
+    testTimeout,
   );
 
-  test("fails if spec request fails", async () => {
-    (axios.default.create as jest.Mock).mockImplementation(() => ({
-      get: jest
-        .fn()
-        .mockImplementationOnce(() =>
-          Promise.resolve({ data: defaultApis, status: 200 }),
-        )
-        .mockImplementationOnce(() =>
-          Promise.resolve({ data: defaultVersions, status: 200 }),
-        )
-        .mockImplementationOnce(() => Promise.reject(new Error("bad wolf"))),
-    }));
-    const checker = new Checker(await getConfig());
-    try {
-      await checker.checkApis();
-      fail("expected error");
-    } catch (err: any) {
-      expect(err.message).toMatch("bad wolf");
-    }
-  });
+  test(
+    "fails if spec request fails",
+    async () => {
+      (axios.default.create as jest.Mock).mockImplementation(() => ({
+        get: jest
+          .fn()
+          .mockImplementationOnce(() =>
+            Promise.resolve({ data: defaultApis, status: 200 }),
+          )
+          .mockImplementationOnce(() =>
+            Promise.resolve({ data: defaultVersions, status: 200 }),
+          )
+          .mockImplementationOnce(() => Promise.reject(new Error("bad wolf"))),
+      }));
+      const checker = new Checker(await getConfig());
+      try {
+        await checker.checkApis();
+        fail("expected error");
+      } catch (err: any) {
+        expect(err.message).toMatch("bad wolf");
+      }
+    },
+    testTimeout,
+  );
 
-  test("fails if spec is invalid", async () => {
-    (axios.default.create as jest.Mock).mockImplementation(() => ({
-      get: jest
-        .fn()
-        .mockImplementationOnce(() =>
-          Promise.resolve({ data: defaultApis, status: 200 }),
-        )
-        .mockImplementationOnce(() =>
-          Promise.resolve({ data: defaultVersions, status: 200 }),
-        )
-        .mockImplementationOnce(() =>
-          Promise.resolve({ data: 42, status: 200 }),
-        ),
-    }));
-    const checker = new Checker(await getConfig());
-    try {
-      await checker.checkApis();
-      fail("expected error");
-    } catch (err: any) {
-      expect(err.message).toMatch(
-        "check 2022-05-23~beta failed with exit code 1",
-      );
-    }
-  });
+  test(
+    "fails if spec is invalid",
+    async () => {
+      (axios.default.create as jest.Mock).mockImplementation(() => ({
+        get: jest
+          .fn()
+          .mockImplementationOnce(() =>
+            Promise.resolve({ data: defaultApis, status: 200 }),
+          )
+          .mockImplementationOnce(() =>
+            Promise.resolve({ data: defaultVersions, status: 200 }),
+          )
+          .mockImplementationOnce(() =>
+            Promise.resolve({ data: 42, status: 200 }),
+          ),
+      }));
+      const checker = new Checker(await getConfig());
+      try {
+        await checker.checkApis();
+        fail("expected error");
+      } catch (err: any) {
+        expect(err.message).toMatch(
+          "check 2022-05-23~beta failed with exit code 1",
+        );
+      }
+    },
+    testTimeout,
+  );
 });
