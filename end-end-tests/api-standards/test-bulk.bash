6a66bc41c3d3a14c6a606ba2d4e256e4ec6450d6
Corneliu Petrescu <corneliu.petrescu@snyk.io>
2024-03-13 15:05:15 +0200

    chore: add tests

--- end-end-tests/api-standards/test-bulk.bash
+++ end-end-tests/api-standards/test-bulk.bash
@@ -38,62 +38,83 @@ function assert_err {
     fi
 }
 
-echo "*** TEST: comparisons that should pass our rules ***"
+# echo "*** TEST: comparisons that should pass our rules ***"
+# init_git_repo
+# mkdir ./components
+# cp $REPO/components/common.yaml ./components/common.yaml
+
+# mkdir -p ./$SPECS_PATH
+
+# cp $REPO/$SPECS_PATH/000-baseline.yaml ./$SPECS_PATH/comparison-1.yaml
+# cp $REPO/$SPECS_PATH/001-ok-add-property-field.yaml ./$SPECS_PATH/comparison-2.yaml
+# cp $REPO/$SPECS_PATH/000-fail-naming.yaml ./$SPECS_PATH/comparison-3.yaml
+# cp $REPO/$SPECS_PATH/000-batch-post.yaml ./$SPECS_PATH/comparison-4.yaml
+# git add .
+# git commit -m 'first commit'
+
+# cp $REPO/$SPECS_PATH/001-ok-add-property-field.yaml ./$SPECS_PATH/comparison-1.yaml
+# cp $REPO/$SPECS_PATH/002-ok-add-operation.yaml ./$SPECS_PATH/comparison-2.yaml
+# cp $REPO/$SPECS_PATH/000-fail-naming.yaml ./$SPECS_PATH/comparison-3.yaml
+# cp $REPO/$SPECS_PATH/000-batch-post.yaml ./$SPECS_PATH/comparison-4.yaml
+# cp $REPO/$SPECS_PATH/001-singleton.yaml ./$SPECS_PATH/comparison-5.yaml
+# cp $REPO/$SPECS_PATH/000-baseline-in-reform.yaml ./$SPECS_PATH/comparison-6.yaml
+
+# git add .
+# git commit -m 'second commit'
+
+# ${COMPARE} --check
+# assert_ok
+
+# echo "*** TEST: comparisons that should fail our rules; expect failure ***"
+
+# init_git_repo
+# mkdir ./components
+# cp $REPO/components/common.yaml ./components/common.yaml
+
+# mkdir -p ./$SPECS_PATH
+
+# cp $REPO/$SPECS_PATH/000-baseline-beta.yaml ./$SPECS_PATH/comparison-2.yaml
+# cp $REPO/$SPECS_PATH/000-baseline-beta.yaml ./$SPECS_PATH/comparison-3.yaml
+# cp $REPO/$SPECS_PATH/000-baseline.yaml ./$SPECS_PATH/comparison-4.yaml
+# cp $REPO/$SPECS_PATH/000-baseline.yaml ./$SPECS_PATH/comparison-5.yaml
+# cp $REPO/$SPECS_PATH/002-ok-add-operation-beta.yaml ./$SPECS_PATH/comparison-6.yaml
+# cp $REPO/$SPECS_PATH/000-baseline.yaml ./$SPECS_PATH/comparison-9.yaml
+
+# git add .
+# git commit -m 'first commit'
+
+# cp $REPO/$SPECS_PATH/000-fail-naming.yaml ./$SPECS_PATH/comparison-1.yaml
+# cp $REPO/$SPECS_PATH/001-fail-format-change-beta.yaml ./$SPECS_PATH/comparison-2.yaml
+# cp $REPO/$SPECS_PATH/001-fail-operation-removed-beta.yaml ./$SPECS_PATH/comparison-3.yaml
+# cp $REPO/$SPECS_PATH/001-fail-operationid-change.yaml ./$SPECS_PATH/comparison-4.yaml
+# cp $REPO/$SPECS_PATH/001-fail-type-change-beta.yaml ./$SPECS_PATH/comparison-5.yaml
+# cp $REPO/$SPECS_PATH/003-fail-type-change-beta.yaml ./$SPECS_PATH/comparison-6.yaml
+# cp $REPO/$SPECS_PATH/002-fail-singleton.yaml ./$SPECS_PATH/comparison-7.yaml
+# cp $REPO/$SPECS_PATH/002-fail-batch-post.yaml ./$SPECS_PATH/comparison-8.yaml
+# cp $REPO/$SPECS_PATH/003-jsonapi.yaml ./$SPECS_PATH/comparison-9.yaml
+# cp $REPO/$SPECS_PATH/002-fail-singleton-no-pagination.yaml ./$SPECS_PATH/comparison-10.yaml
+
+# git add .
+# git commit -m 'second commit'
+
+# ${COMPARE} --check
+# assert_err
+
+
+echo "*** TEST: diff-all expect failure on invalid api even if no x-optic-url specified if not --upload option ***"
+
 init_git_repo
 mkdir ./components
 cp $REPO/components/common.yaml ./components/common.yaml
 
 mkdir -p ./$SPECS_PATH
 
-cp $REPO/$SPECS_PATH/000-baseline.yaml ./$SPECS_PATH/comparison-1.yaml
-cp $REPO/$SPECS_PATH/001-ok-add-property-field.yaml ./$SPECS_PATH/comparison-2.yaml
-cp $REPO/$SPECS_PATH/000-fail-naming.yaml ./$SPECS_PATH/comparison-3.yaml
-cp $REPO/$SPECS_PATH/000-batch-post.yaml ./$SPECS_PATH/comparison-4.yaml
-git add .
-git commit -m 'first commit'
-
-cp $REPO/$SPECS_PATH/001-ok-add-property-field.yaml ./$SPECS_PATH/comparison-1.yaml
-cp $REPO/$SPECS_PATH/002-ok-add-operation.yaml ./$SPECS_PATH/comparison-2.yaml
-cp $REPO/$SPECS_PATH/000-fail-naming.yaml ./$SPECS_PATH/comparison-3.yaml
-cp $REPO/$SPECS_PATH/000-batch-post.yaml ./$SPECS_PATH/comparison-4.yaml
-cp $REPO/$SPECS_PATH/001-singleton.yaml ./$SPECS_PATH/comparison-5.yaml
-cp $REPO/$SPECS_PATH/000-baseline-in-reform.yaml ./$SPECS_PATH/comparison-6.yaml
-
-git add .
-git commit -m 'second commit'
-
-${COMPARE} --check
-assert_ok
-
-echo "*** TEST: comparisons that should fail our rules; expect failure ***"
-
-init_git_repo
-mkdir ./components
-cp $REPO/components/common.yaml ./components/common.yaml
-
-mkdir -p ./$SPECS_PATH
-
-cp $REPO/$SPECS_PATH/000-baseline-beta.yaml ./$SPECS_PATH/comparison-2.yaml
-cp $REPO/$SPECS_PATH/000-baseline-beta.yaml ./$SPECS_PATH/comparison-3.yaml
-cp $REPO/$SPECS_PATH/000-baseline.yaml ./$SPECS_PATH/comparison-4.yaml
-cp $REPO/$SPECS_PATH/000-baseline.yaml ./$SPECS_PATH/comparison-5.yaml
-cp $REPO/$SPECS_PATH/002-ok-add-operation-beta.yaml ./$SPECS_PATH/comparison-6.yaml
-cp $REPO/$SPECS_PATH/000-baseline.yaml ./$SPECS_PATH/comparison-9.yaml
+cp $REPO/$SPECS_PATH/005-baseline-no-optic-url.yaml ./$SPECS_PATH/comparison-1.yaml
 
 git add .
 git commit -m 'first commit'
 
-cp $REPO/$SPECS_PATH/000-fail-naming.yaml ./$SPECS_PATH/comparison-1.yaml
-cp $REPO/$SPECS_PATH/001-fail-format-change-beta.yaml ./$SPECS_PATH/comparison-2.yaml
-cp $REPO/$SPECS_PATH/001-fail-operation-removed-beta.yaml ./$SPECS_PATH/comparison-3.yaml
-cp $REPO/$SPECS_PATH/001-fail-operationid-change.yaml ./$SPECS_PATH/comparison-4.yaml
-cp $REPO/$SPECS_PATH/001-fail-type-change-beta.yaml ./$SPECS_PATH/comparison-5.yaml
-cp $REPO/$SPECS_PATH/003-fail-type-change-beta.yaml ./$SPECS_PATH/comparison-6.yaml
-cp $REPO/$SPECS_PATH/002-fail-singleton.yaml ./$SPECS_PATH/comparison-7.yaml
-cp $REPO/$SPECS_PATH/002-fail-batch-post.yaml ./$SPECS_PATH/comparison-8.yaml
-cp $REPO/$SPECS_PATH/003-jsonapi.yaml ./$SPECS_PATH/comparison-9.yaml
-cp $REPO/$SPECS_PATH/002-fail-singleton-no-pagination.yaml ./$SPECS_PATH/comparison-10.yaml
-
+cp $REPO/$SPECS_PATH/005-fail-breaking-param-change-beta ./$SPECS_PATH/comparison-1.yaml
 git add .
 git commit -m 'second commit'
 
