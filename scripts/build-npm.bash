4421776528a61d546e5a771393936cd57d8582b6
RaduPetreTarean <156008349+RaduPetreTarean@users.noreply.github.com>
2026-08-27 11:46:18 +0300

    fix: migrate to NPM OIDC trusted publishing (#744)

--- scripts/build-npm.bash
+++ scripts/build-npm.bash
@@ -6,11 +6,3 @@ cd $(dirname $0)/..
 npm ci
 npm run build
 npm pack
-
-# Use GH_TOKEN as a fallback if GITHUB_TOKEN is not set
-export GITHUB_TOKEN=${GITHUB_TOKEN:-$GH_TOKEN}
-
-if [[ -n "$GITHUB_TOKEN" && -n "$NPM_TOKEN" ]]; then
-  # fix dependency at 22.0.12 untill we find workaround for node20 requirement
-  npx "semantic-release@22.0.12"
-fi
